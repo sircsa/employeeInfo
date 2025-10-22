95ec690c2c26ca67d9ee784efc92f8ad4dcaa136
Serghei <serghei.cerneaev@gmail.com>
2025-10-22 17:17:13 -0400

    minor changes

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000},
-    {id: 1, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000},
-    {id: 1, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000}
+    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript'},
+    {id: 1, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python'},
+    {id: 1, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java'}
 
 ]
 
@@ -29,6 +29,16 @@ function findEmployeeById(employeeId) {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee) {
     document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+     }
+ }
+
+ function findEmployeeBySpec(specialization) {
+    const foundEmployee = employees.find(employee => employee.specialization === specialization);
+    if (foundEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.specialization}</p>`;
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
